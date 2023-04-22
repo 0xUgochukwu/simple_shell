1465b4924b27663d4481a369304c648fe4e411f6
0xUgochukwu <0xUgochukwu@gmail.com>
2023-04-22 03:05:52 -0700

    fixed non-interactive mode

--- main.c
+++ main.c
@@ -11,6 +11,10 @@ int main (int __attribute__((unused)) ac, char **av)
 {
 	char *err_msg = malloc(1024);
 	int err_len;
+	char *command;
+	size_t len = 0;
+	ssize_t read_bytes;
+	command_t *cmds;
 
 	err_len = sprintf(err_msg, "Usage: %s or %s [filename]\n", av[0], av[0]);
 
@@ -20,7 +24,24 @@ int main (int __attribute__((unused)) ac, char **av)
 		return (0);
 	}
 	else
-		prompt();
+	{
+		if (!isatty(STDIN_FILENO))
+		{
+			fflush(stdout);
+			read_bytes = getline(&command, &len, stdin);
+
+			if (read_bytes == -1)
+				exit(0);
+
+			command[strcspn(command, "\n")] = '\0';
+
+			cmds = parse_command(command);
+
+			execute_commands(cmds);
+		}
+		else
+			prompt();
+	}
 
 	free(err_msg);
 	return (0);
