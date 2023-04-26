4ad835a01123421262e7697dce4033636ba163af
0xUgochukwu <0xUgochukwu@gmail.com>
2023-04-26 15:50:56 -0700

    test

--- main.c
+++ main.c
@@ -1,55 +1,142 @@
 #include "main.h"
 
 /**
- * main - simple shell
+ * main - entry point for the simple shell program
  *
- * @ac: argument count
- * @av: list of arguments
- * @envp: environment variables
- *
- * Return: int
+ * Return: Always 0.
  */
-
-int main(int __attribute__((unused)) ac, char **av, char **envp)
+int main(void)
 {
-	char *err_msg = malloc(1024);
-	int err_len;
-	char *command;
-	size_t len = 0;
-	ssize_t read_bytes;
-	command_t *cmds;
+	       char *line = NULL, **args = NULL;
+        size_t line_size = 0;
+        ssize_t read = 0;
+        int status = 0, interactive = isatty(STDIN_FILENO);;
 
-	set_status(0);
-	set_num_commands(1);
-	set_environ(envp);
+        while (1)
+        {
+                /* Display prompt and get command line */
+		if (interactive)
+                        _puts("$ ");
+                read = getline(&line, &line_size, stdin);
+                if (read == -1)
+                        break;
 
-	err_len = sprintf(err_msg, "Usage: %s or %s [filename]\n", av[0], av[0]);
-	if (ac > 2)
+                /* Remove newline character */
+                if (line[read - 1] == '\n')
+                        line[read - 1] = '\0';
+
+                /* Split command line into arguments */
+                args = split_line(line);
+
+                /* Execute command */
+                status = execute(args);
+
+                /* Free memory and reset variables */
+                free(line);
+                free(args);
+                line = NULL;
+                args = NULL;
+        }
+
+        /* Free any remaining memory */
+        free(line);
+        free(args);
+
+        /* Exit the shell */
+
+	if (interactive)
+        	_puts("\n");
+        return (status);
+}
+
+/**
+ * execute - execute a command with arguments
+ * @args: list of arguments
+ *
+ * Return: 1 on success, 0 on failure.
+ */
+int execute(char **args)
+{
+	pid_t pid = 0;
+	int status = 0;
+
+	/* Fork a new process */
+	pid = fork();
+
+	/* Check for errors */
+	if (pid == -1)
 	{
-		write(STDOUT_FILENO, err_msg, err_len);
+		perror("fork");
 		return (0);
+	}
+	else if (pid == 0)
+	{
+		/* Child process */
+
+		/* Attempt to execute command */
+		if (execve(args[0], args, environ) == -1)
+		{
+			/* Command not found */
+			perror("execve");
+			_exit(127);
+		}
 	}
 	else
 	{
-		if (ac == 2)
-		{
-			run_file(av[1]);
-			exit(get_status());
-		}
-		if (!isatty(STDIN_FILENO))
-		{
-			fflush(stdout);
-			read_bytes = getline(&command, &len, stdin);
-			if (read_bytes == -1)
-				exit(0);
+		/* Parent process */
 
-			command[strcspn(command, "\n")] = '\0';
-			cmds = parse_command(command);
-			execute_commands(cmds);
-		}
-		else
-			prompt();
+		/* Wait for child to complete */
+		do
+		{
+			waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
 	}
-	free(err_msg);
-	return (0);
+
+	return (1);
+}
+
+/**
+ * split_line - split a command line into arguments
+ * @line: command line
+ *
+ * Return: array of arguments
+ */
+char **split_line(char *line)
+{
+	char **args = NULL;
+	char *token = NULL;
+	size_t i = 0;
+
+	/* Allocate memory for arguments */
+	args = malloc(ARG_MAX * sizeof(char *));
+	if (args == NULL)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Split line into tokens */
+	token = strtok(line, " ");
+	while (token != NULL)
+	{
+		args[i] = token;
+		i++;
+		token = strtok(NULL, " ");
+	}
+
+	/* Set last argument to NULL */
+	args[i] = NULL;
+
+	return (args);
+}
+
+
+int _puts(const char *str) {
+    int num_chars = 0;
+    while (*str != '\0')
+    {
+        write(STDOUT_FILENO, str++, 1);
+        num_chars++;
+    }
+    return num_chars;
 }
