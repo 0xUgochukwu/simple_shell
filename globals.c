7a531f5cf9a6b23a98d14e8611bbcd0552458ecf
0xUgochukwu <0xUgochukwu@gmail.com>
2023-04-25 17:09:29 -0700

    checkers please be nice

--- globals.c
+++ globals.c
@@ -1,31 +1,42 @@
 #include "main.h"
 
+
+/**
+ * set_status - sets status global variable
+ * @status: status to set to
+ */
+
 void set_status(int status)
 {
 	g_main_status = status;
 }
+
+/**
+ * get_status - gets the status global variable
+ * Return: global status variable
+ */
 
 int get_status(void)
 {
 	return (g_main_status);
 }
 
+
+/**
+ * set_num_commands - sets num_commands global variable
+ * @num: number to set to
+ */
+
 void set_num_commands(int num)
 {
 	g_num_commands = num;
 }
 
+/**
+ * get_num_commands - gets the num_commands global variable
+ * Return: global num_commands variable
+ */
 int get_num_commands(void)
 {
 	return (g_num_commands);
-}
-
-void set_environ(char **envp)
-{
-	g_environ = envp;
-}
-
-char **get_environ(void)
-{
-	return (g_environ);
 }
--- split.c
+++ split.c
@@ -4,48 +4,47 @@
  * split - split a command into an array of commands
  * @cmd: command to split
  *
- * Return: pointer to array of commands
+ * Return: pointer to array of commandss
  */
 
 command_t *split(char *cmd)
 {
-	int argc = 0, i, num_commands = 1;
+	int argc = 0, n = 1;
 	char *token;
-	command_t *commands = malloc(sizeof(command_t));
+	command_t *cmds = malloc(sizeof(command_t));
 
-	commands[0].op = NULL;
-	commands[0].argv = malloc(sizeof(char *));
+	n = 1;
+	cmds[0].op = NULL;
+	cmds[0].argv = malloc(sizeof(char *));
 	token = strtok(cmd, " ");
 	while (token != NULL)
 	{
-		i = num_commands - 1;
 		if (*token == '#')
 			break;
 		if (*token == ';' || *token == '&' || *token == '|')
 		{
 			if (argc > 0)
-				commands[i].cmd = commands[i].argv[0];
+				cmds[n - 1].cmd = cmds[n - 1].argv[0];
 			else
-				commands[i].cmd = NULL;
-
-			commands = realloc(commands, (++num_commands) * sizeof(command_t));
-			commands[i++].op = (*token == ';') ? NULL : token;
-			commands[i].argv = malloc(sizeof(char *));
+				cmds[n - 1].cmd = NULL;
+			cmds = realloc(cmds, (++n) * sizeof(command_t));
+			cmds[n - 1].op = (*token == ';') ? NULL : token;
+			cmds[n - 1].argv = malloc(sizeof(char *));
 			argc = 0;
 		}
 		else
 		{
-			commands[i].argv = realloc(commands[i].argv, (argc + 2) * sizeof(char *));
-			commands[i].argv[argc++] = strdup(token);
-			commands[i].argv[argc] = NULL;
+			cmds[n - 1].argv = realloc(cmds[n - 1].argv, (argc + 2) * sizeof(char *));
+			cmds[n - 1].argv[argc++] = strdup(token);
+			cmds[n - 1].argv[argc] = NULL;
 		}
+
 		token = strtok(NULL, " ");
 	}
 	if (argc > 0)
-		commands[num_commands - 1].cmd = commands[num_commands - 1].argv[0];
+		cmds[n - 1].cmd = cmds[n - 1].argv[0];
 	else
-		commands[num_commands - 1].cmd = NULL;
-	set_num_commands(num_commands);
-
-	return (commands);
+		cmds[n - 1].cmd = NULL;
+	set_num_commands(n);
+	return (cmds);
 }
