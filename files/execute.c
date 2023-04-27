a25f0a343341adb67644c542f91259af5f432841
0xUgochukwu <0xUgochukwu@gmail.com>
2023-04-26 20:10:04 -0700

    tester

---
+++ files/execute.c
@@ -0,0 +1,171 @@
+#include "main.h"
+
+/**
+ * execute_commands - executes all comands from user
+ * @cmds: array of all comands
+ *
+ * Return: void
+ */
+
+int execute_commands(command_t *cmds)
+{
+	int i, status, op_check, num_commands = get_num_commands();
+
+	for (i = 0; i < num_commands; i++)
+	{
+		if (cmds[i].op == NULL)
+			status = execute_cmd(cmds[i]);
+		else
+		{
+			op_check = operator_check(cmds[i].op, status);
+			if (op_check)
+				status = execute_cmd(cmds[i]);
+			else
+				return (status);
+		}
+	}
+
+	set_status(status);
+
+	return (status);
+}
+
+/**
+ * operator_check - checks using an operator if the next command
+ * should be executed or not
+ * @op: operator
+ * @status: status of previous command
+ * Return: 1 if next should be executed, 0 if it shouldn't
+ */
+
+int operator_check(char *op, int status)
+{
+	if (_strcmp(op, "&&") == 0)
+	{
+		if (status == 0)
+			return (1);
+		else
+			return (0);
+	}
+	else if (_strcmp(op, "||") == 0)
+	{
+		if (status != 0)
+			return (1);
+		else
+			return (0);
+	}
+	else
+		return (1);
+}
+
+/**
+ * execute_cmd - execute cmd
+ * @cmd_s: command struct
+ *
+ * Return: void
+ */
+
+int execute_cmd(command_t cmd_s)
+{
+	char *path = get_path(cmd_s.cmd), **environ = get_environ();
+	int status = -1;
+	pid_t pid;
+
+	replace_vars(cmd_s);
+	status = builtin_check(cmd_s.cmd, cmd_s.argv);
+
+	if (status >= 0)
+		return (status);
+	if (path == NULL)
+		return (throw_error("path"));
+	pid = fork();
+	if (pid == -1)
+		return (throw_error("fork"));
+	if (pid == 0)
+	{
+		execve(path, cmd_s.argv, environ);
+		perror("execvp");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		if (waitpid(pid, &status, 0) == -1)
+			return (throw_error("waitpid"));
+		if (WIFEXITED(status))
+		{
+			if (WEXITSTATUS(status) != 0)
+			{
+				perror(cmd_s.cmd);
+				return (WEXITSTATUS(status));
+			}
+		}
+		else
+		{
+			perror(cmd_s.cmd);
+			return (status);
+		}
+	}
+	return (status);
+}
+
+/**
+ * get_path - gets the full path of a command
+ * @cmd: command to get path
+ * Return: full path of cmd or null
+ */
+
+char *get_path(char *cmd)
+{
+	char *path = _getenv("PATH");
+	char *token = strtok(_strdup(path), ":");
+	struct stat st;
+	char *full_path = NULL;
+
+	if (stat(cmd, &st) == 0 && st.st_mode & S_IXUSR)
+		return (cmd);
+
+	while (token != NULL)
+	{
+		full_path = str_concat(token, cmd, '/');
+
+		if (stat(full_path, &st) == 0 && st.st_mode & S_IXUSR)
+			return (full_path);
+
+		free(full_path);
+		full_path = NULL;
+
+		token = strtok(NULL, ":");
+	}
+	return (NULL);
+}
+
+
+/**
+ * builtin_check - check if command is a builtin
+ * @cmd: command to run
+ * @args: arguments
+ *
+ * Return: status
+ */
+
+int builtin_check(char *cmd, char **args)
+{
+	cmds_t bi_cmds[] = {
+		{"unsetenv", bi_unsetenv},
+		{"setenv", bi_setenv},
+		{"alias", bi_alias},
+		{"exit", bi_exit},
+		{"env", bi_env},
+		{"cd", bi_cd},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (i < 6)
+	{
+		if (_strcmp(cmd, bi_cmds[i].cmd) == 0)
+			return (bi_cmds[i].fnc(args));
+		i++;
+	}
+	return (-1);
+}
---
+++ files/main.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * main - simple shell
+ *
+ * @ac: argument count
+ * @av: list of arguments
+ * @envp: environment variables
+ *
+ * Return: int
+ */
+
+int main(int __attribute__((unused)) ac, char **av, char **envp)
+{
+	char *err_msg = malloc(1024);
+	int err_len;
+	char *command;
+	size_t len = 0;
+	ssize_t read_bytes;
+	command_t *cmds;
+
+	signal(SIGINT, sig_handler);
+	set_status(0);
+	set_num_commands(1);
+	set_environ(envp);
+
+	err_len = sprintf(err_msg, "Usage: %s or %s [filename]\n", av[0], av[0]);
+	if (ac > 2)
+	{
+		write(STDOUT_FILENO, err_msg, err_len);
+		return (0);
+	}
+	else
+	{
+		if (!isatty(STDIN_FILENO))
+		{
+			fflush(stdout);
+			read_bytes = getline(&command, &len, stdin);
+			if (read_bytes == -1)
+				exit(0);
+
+			command[strcspn(command, "\n")] = '\0';
+			cmds = parse_command(command);
+			execute_commands(cmds);
+		}
+		else
+			prompt();
+	}
+	free(err_msg);
+	return (0);
+}
